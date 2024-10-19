7a42c377caecb0be880506b6be7b2f141229bd24
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-10-20 00:21:24 +0700

    membuat class main untuk aktifasi aplikasi

---
+++ app/src/main/java/com/example/composablelayout/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.composablelayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composablelayout.ui.theme.ComposableLayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposableLayoutTheme {
+                // Menggunakan Scaffold untuk struktur tata letak yang konsisten
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicCompos(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
